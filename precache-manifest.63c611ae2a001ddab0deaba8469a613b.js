3460e3e63073105b33cfc0df8f082586e0c0285b
Riijohanna <riikka.jharju@gmail.com>
2019-05-08 18:01:10 +0300

    Updates

---
+++ precache-manifest.63c611ae2a001ddab0deaba8469a613b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15be4cd6b58ae72dc3d6",
+    "url": "/Speedgame/static/js/runtime~main.fcc3f77f.js"
+  },
+  {
+    "revision": "1d7eba8710e839293c58",
+    "url": "/Speedgame/static/js/main.294f20e2.chunk.js"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/Speedgame/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "1d7eba8710e839293c58",
+    "url": "/Speedgame/static/css/main.276f29e3.chunk.css"
+  },
+  {
+    "revision": "19700bd70da27ac6201fec6651311464",
+    "url": "/Speedgame/index.html"
+  }
+];
